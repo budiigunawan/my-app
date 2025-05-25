66cca5df6d204a417aabc51feb3e48866d9b3dbf
Budi Gunawan <budiindra.g@gmail.com>
2025-05-25 12:42:13 +0700

    change value to default value in input

--- src/components/my-profile/additional-details.tsx
+++ src/components/my-profile/additional-details.tsx
@@ -11,19 +11,19 @@ export const AdditionalDetails = ({ isMobile }: AdditionalDetailsProps) => {
         <Field.Label fontWeight={'bold'}>
           Home address <Field.RequiredIndicator color={'black'} />
         </Field.Label>
-        <Input variant={'flushed'} border={0} value={'Sesame Street'} />
+        <Input variant={'flushed'} border={0} defaultValue={'Sesame Street'} />
       </Field.Root>
       <Field.Root required>
         <Field.Label fontWeight={'bold'}>
           Country <Field.RequiredIndicator color={'black'} />
         </Field.Label>
-        <Input variant={'flushed'} border={0} value={'Indonesia'} />
+        <Input variant={'flushed'} border={0} defaultValue={'Indonesia'} />
       </Field.Root>
       <Field.Root required>
         <Field.Label fontWeight={'bold'}>
           Postal code <Field.RequiredIndicator color={'black'} />
         </Field.Label>
-        <Input variant={'flushed'} border={0} value={'53257'} />
+        <Input variant={'flushed'} border={0} defaultValue={'53257'} />
       </Field.Root>
       <Field.Root>
         <Field.Label fontWeight={'bold'}>Date of birth</Field.Label>
@@ -31,16 +31,16 @@ export const AdditionalDetails = ({ isMobile }: AdditionalDetailsProps) => {
           type="date"
           variant={'flushed'}
           border={0}
-          value={'1996-07-31'}
+          defaultValue={'1996-07-31'}
         />
       </Field.Root>
       <Field.Root>
         <Field.Label fontWeight={'bold'}>Gender</Field.Label>
-        <Input variant={'flushed'} border={0} value={'Male'} />
+        <Input variant={'flushed'} border={0} defaultValue={'Male'} />
       </Field.Root>
       <Field.Root>
         <Field.Label fontWeight={'bold'}>Marital status</Field.Label>
-        <Input variant={'flushed'} border={0} value={'Married'} />
+        <Input variant={'flushed'} border={0} defaultValue={'Married'} />
       </Field.Root>
     </Stack>
   );
--- src/components/my-profile/spouse-details.tsx
+++ src/components/my-profile/spouse-details.tsx
@@ -9,15 +9,15 @@ export const SpouseDetails = ({ isMobile }: SpouseDetailsProps) => {
     <Stack p={isMobile ? '16px' : 0}>
       <Field.Root>
         <Field.Label fontWeight={'bold'}>Salutation</Field.Label>
-        <Input variant={'flushed'} border={0} value={'Mrs.'} />
+        <Input variant={'flushed'} border={0} defaultValue={'Mrs.'} />
       </Field.Root>
       <Field.Root>
         <Field.Label fontWeight={'bold'}>First name</Field.Label>
-        <Input variant={'flushed'} border={0} value={'Dita'} />
+        <Input variant={'flushed'} border={0} defaultValue={'Dita'} />
       </Field.Root>
       <Field.Root>
         <Field.Label fontWeight={'bold'}>Last name</Field.Label>
-        <Input variant={'flushed'} border={0} value={'Gunawan'} />
+        <Input variant={'flushed'} border={0} defaultValue={'Gunawan'} />
       </Field.Root>
     </Stack>
   );
